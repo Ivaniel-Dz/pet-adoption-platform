c15c244fc2d2f6cfbc43ffcf6bfdf0f8949372db
Ivaniel-Dz <pcivaniel@gmail.com>
2026-02-15 15:12:20 -0500

    fix view admin user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,21 +2,24 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
 
-
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
 
     list_display = ('email', 'username', 'role', 'is_staff', 'is_active')
     list_filter = ('role', 'is_staff', 'is_active')
 
-    fieldsets = UserAdmin.fieldsets + (('Información adicional', {
-        'fields': ('role', 'phone', 'address'),
-    }),
+    fieldsets = UserAdmin.fieldsets + (
+        ('Información adicional', {
+            'fields': ('role', 'phone', 'address'),
+        }),
     )
 
-    add_fieldsets = UserAdmin.add_fieldsets + (('Información adicional', {
-        'fields': ('role', 'phone', 'address'),
-    }),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Información adicional', {
+            'fields': ('email', 'role', 'phone', 'address'),
+        }),
     )
+
+    ordering = ('email',)
