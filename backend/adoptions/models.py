bea648f0bca7da41672ebbf2559d055e0087f531
Ivaniel-Dz <pcivaniel@gmail.com>
2026-02-18 16:49:26 -0500

    creación del modelo Adoptions

--- backend/adoptions/models.py
+++ backend/adoptions/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
+from pets.models import Pet
 
-# Create your models here.
+
+class Adoption(models.Model):
+
+    STATUS_CHOICES = [
+        ('PENDING', 'Pendiente'),
+        ('APPROVED', 'Aprobado'),
+        ('REJECT', 'Rechazado'),
+    ]
+
+    # Usuario que realiza la solicitud
+    user = models.ForeignKey(
+        # asegura compatibilidad con CustomUser
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='adoptions'
+    )
+
+    # Mascota Solicitada
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name='adoption_request'
+    )
+
+    # Estado de la solicitud
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    # Fecha de creación automática
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Evita duplicar solicitudes PENDIENTES para la misma mascota por el mismo usuario
+    class Meta:
+        unique_together = ('user', 'pet')
+
+        def __str__(self):
+            return f"{self.user.username} -> {self.pet.name} ({self.status})"
