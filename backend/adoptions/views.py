9f17a2871394c43dd1bc1e8ab31b846061951b53
Ivaniel-Dz <pcivaniel@gmail.com>
2026-02-18 17:11:48 -0500

    config view for adoptions

--- backend/adoptions/views.py
+++ backend/adoptions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Adoption
+from .serializers import AdoptionSerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class AdoptionViewSet(viewsets.ModelViewSet):
+    queryset = Adoption.objects.all()
+    serializer_class = AdoptionSerializer
+    permission_classes = [IsAuthenticated & IsAdminOrReadOnly]
+
+    #  Se ejecuta automáticamente al hacer POST. Aquí asignamos el usuario autenticado.
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    #  Se ejecuta cuando un admin cambia el estado.
+    def perform_update(self, serializer):
+        adoption = serializer.save()
+
+        # Si fuera aprobado
+        if adoption.status == 'APPROVED':
+            pet = adoption.pet
+            pet.available = False
+            pet.save()
