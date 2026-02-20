72ad40b805e7ef236452eeb536bd5ab9a9699a31
Ivaniel-Dz <pcivaniel@gmail.com>
2026-02-20 15:40:51 -0500

    add filter in pets

--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Pet
 from .serializers import PetSerializer
 
@@ -13,6 +15,15 @@ class PetViewSet(viewsets.ModelViewSet):
     # Definimos el queryset y el serializer para el ViewSet.
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+
+    # Filtros habilitados
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # Filtrar por estado y especie
+    filter_fields = ['status', 'species']
+    # Búsqueda por nombre
+    search_fields = ['name']
+    # Ordenamiento permitido
+    ordering_fields = ['created_at', 'age', 'name']
 
     # Definimos permisos personalizados según la acción.
     def get_permissions(self):
