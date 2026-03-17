381d4b7797fe63ac63d4518698849ff99bb33590
Ivaniel-Dz <pcivaniel@gmail.com>
2026-03-16 23:08:35 -0500

    config router app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+// Importamos los componentes o paginas que queremos usar en las rutas
+import { LandingPage } from './pages/landing/landing-page/landing-page';
 
-export const routes: Routes = [];
+// Definimos las rutas de la aplicación
+export const routes: Routes = [
+    {
+        path: '', // Ruta raíz, se muestra la Landing Page
+        component: LandingPage, // Componente que se renderiza para esta ruta
+        title: 'Landing Page' // Título de la página, se muestra en la pestaña del navegador
+    }
+];
