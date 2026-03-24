ef324893cbc902ba822db98d110d6d41c8f6ef65
Ivaniel-Dz <pcivaniel@gmail.com>
2026-03-24 01:54:31 -0500

    update link

--- frontend/src/app/layouts/navigation/navigation.ts
+++ frontend/src/app/layouts/navigation/navigation.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
 export class Navigation {
-
+  
 }
