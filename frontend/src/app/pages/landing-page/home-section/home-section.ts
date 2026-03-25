dc134b25b258668d205daac511247b9838340023
Ivaniel-Dz <pcivaniel@gmail.com>
2026-03-25 01:54:15 -0500

    update estilos pets-section

--- frontend/src/app/pages/landing-page/home-section/home-section.ts
+++ frontend/src/app/pages/landing-page/home-section/home-section.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-section',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home-section.scss',
 })
 export class HomeSection {
+  private router = inject(Router)
 
+  gotToPets(){
+    (document.activeElement as HTMLElement)?.blur();// Quita el foco del botón para evitar que quede resaltado después de hacer clic
+    this.router.navigate(['/pets'])
+  }
 }
