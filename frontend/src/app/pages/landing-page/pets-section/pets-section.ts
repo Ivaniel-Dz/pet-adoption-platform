1477eb6c780a0f2552043326c1c5f329a2fb7297
Ivaniel-Dz <pcivaniel@gmail.com>
2026-03-25 00:31:22 -0500

    fix: clases de html pets-page

--- frontend/src/app/pages/landing-page/pets-section/pets-section.ts
+++ frontend/src/app/pages/landing-page/pets-section/pets-section.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PetsService } from '../../../services/pets-service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Pet } from '../../../interfaces/pet';
 
 @Component({
   selector: 'app-pets-section',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pets-section.html',
   styleUrl: './pets-section.scss',
 })
-export class PetsSection {
+export class PetsSection implements OnInit {
+  // Inyectamos los servicios
+  private petsService = inject(PetsService);
+  // Inyectamos el Router para navegar a la página de detalles de la mascota
+  private router = inject(Router);
 
+  pets: Pet[] = [];
+
+  ngOnInit() {
+
+  }
+
+  goToDetail(id: number) {
+    this.router.navigate(['/pet', id]);
+  }
 }
