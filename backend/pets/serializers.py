f03deefbb1fc13f5bc3c337365158bb5429426ba
Ivaniel-Dz <pcivaniel@gmail.com>
2026-03-25 00:04:07 -0500

    add variables de solo lectura para de la serializer Pets

--- backend/pets/serializers.py
+++ backend/pets/serializers.py
@@ -3,6 +3,13 @@ from .models import Pet
 
 # Serializador para el modelo Pet
 class PetSerializer(serializers.ModelSerializer):
+    # Define un campo de solo lectura
+    species_display = serializers.CharField(source='get_species_display', read_only=True)
+    size_display = serializers.CharField(source='get_size_display', read_only=True)
+    status_display = serializers.CharField(source='get_status_display', read_only=True)
+
+
+    # Especifica el modelo y los campos a incluir en la serialización
     class Meta:
         # El modelo que se va a serializar
         model = Pet
