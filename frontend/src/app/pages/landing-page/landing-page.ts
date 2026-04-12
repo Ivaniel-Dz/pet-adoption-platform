139ee4b4125b4df65b4e32b010645fbf2a77b778
Ivaniel-Dz <pcivaniel@gmail.com>
2026-04-12 02:01:18 -0500

    update style landingPage

--- frontend/src/app/pages/landing-page/landing-page.ts
+++ frontend/src/app/pages/landing-page/landing-page.ts
@@ -3,10 +3,13 @@ import { Navigation } from '../../layouts/navigation/navigation';
 import { HomeSection } from './home-section/home-section';
 import { PetsSection } from './pets-section/pets-section';
 import { Footer } from '../../layouts/footer/footer';
+import { AboutSection } from './about-section/about-section';
+import { CtasSection } from './ctas-section/ctas-section';
+import { ContactSection } from './contact-section/contact-section';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Navigation, HomeSection, PetsSection, Footer, ],
+  imports: [Navigation, HomeSection, PetsSection, AboutSection, CtasSection, ContactSection ,Footer, ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
