9b3481dc99418a26317bccf4e1a861454293ae36
Ivaniel-Dz <pcivaniel@gmail.com>
2026-04-12 02:03:58 -0500

    add ctas section

---
+++ frontend/src/app/pages/landing-page/ctas-section/ctas-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ctas-section',
+  imports: [],
+  templateUrl: './ctas-section.html',
+  styleUrl: './ctas-section.scss',
+})
+export class CtasSection {
+
+}
