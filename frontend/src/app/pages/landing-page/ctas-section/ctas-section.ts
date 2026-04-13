a8691e4599b42767961d10e0d23bcb9e329b9ff7
Ivaniel-Dz <pcivaniel@gmail.com>
2026-04-12 23:03:35 -0500

    update ctas, routerlink

--- frontend/src/app/pages/landing-page/ctas-section/ctas-section.ts
+++ frontend/src/app/pages/landing-page/ctas-section/ctas-section.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ctas-section',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ctas-section.html',
   styleUrl: './ctas-section.scss',
 })
