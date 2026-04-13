39b962224ed1574b3590a08afc52ead55b774e56
Ivaniel-Dz <pcivaniel@gmail.com>
2026-04-12 21:59:28 -0500

    add page adoption-info

---
+++ frontend/src/app/pages/adoption-info-page/adoption-info-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Navigation } from '../../layouts/navigation/navigation';
+import { Footer } from '../../layouts/footer/footer';
+
+@Component({
+  selector: 'app-adoption-info-page',
+  imports: [Navigation, Footer],
+  templateUrl: './adoption-info-page.html',
+  styleUrl: './adoption-info-page.scss',
+})
+export class AdoptionInfoPage {
+
+}
