08cc1692c3420ac7a45aa33588e445208b1e9347
Ivaniel-Dz <pcivaniel@gmail.com>
2026-05-11 01:32:37 -0500

    update model user

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,13 +6,17 @@ class User(AbstractUser):
     
     ROLE_CHOICES = (('ADMIN', 'Admin'), ('ADOPTANTE', 'Adoptante'),)
 
+    username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='ADOPTANTE')
     phone = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
 
+    # Login con email
     USERNAME_FIELD = 'email'
+    # Campos requeridos al crear superusuario
     REQUIRED_FIELDS = ['username']
 
+    # Mostrar el username junto con el email en la representación del usuario
     def __str__(self):
-        return self.email
+        return f'{self.username} ({self.email})' 
