12eee5ea40888f59b9047e7abce6ba907d7902a1
Ivaniel-Dz <pcivaniel@gmail.com>
2026-05-11 18:29:14 -0500

    add and config authServices

--- frontend/README.md
+++ frontend/README.md
@@ -11,17 +11,17 @@ npx -p @angular/cli@17 ng new [nameProject]
 ```
 > Ultima version 
 ```bash
-npx @angular/cli@latest new [nameProject]
+ng new my-app
 ```
 
 > Verificar version instalado
 ```bash
-npx ng version
+ng version
 ```
 
 ## Ejecución del servidor
 ```bash
-npx ng serve
+ng serve
 ```
 
 ## Instalar dependencias necesarias
@@ -30,3 +30,53 @@ npm install
 ```
 
 > A partir de Angular 22, no funciona las Directivas: ``ngFor`` y ``ngIF``, usar ``Signal`` para evitar bugs de carga de datos con el uso de las directivas ``@for`` y ``@if``
+
+## Notas
+### ¿Guardar el access token en un signal() es seguro?
+
+- Sí, es mucho más seguro que localStorage o sessionStorage, porque el token vive solo en memoria (RAM).
+
+```bash
+accessToken = signal<string | null>(null);
+```
+
+Ventajas:
+✅ No persiste en navegador
+✅ Se elimina al cerrar pestaña/refresh
+✅ No queda almacenado permanentemente
+✅ Menor riesgo de robo comparado con localStorage
+
+### Entonces ¿cuál es la arquitectura más segura?
+La recomendación moderna para SPA (Angular/React/Vue) es:
+```bash
+Refresh Token
+↓
+HttpOnly Cookie (backend)
+
+Access Token
+↓
+Memoria RAM (signal/store)
+```
+O sea exactamente lo que implementamos en el proyecto.
+
+Arquitectura del Proyecto
+```bash
+Django
+├── refresh token (HttpOnly cookie)
+└── access token (JSON response)
+
+Angular
+├── access token -> signal()
+└── nunca toca refresh token
+```
+
+> ¿Se puede usar HttpOnly Cookie en frontend?
+> NO.
+> HttpOnly solo existe del lado del backend.
+
+### ¿Entonces Angular cómo usa la cookie?
+Automáticamente. Cuando se hace:
+```bash
+withCredentials: true
+```
+> el navegador envía la cookie solo. Angular no necesita tocar la cookie.
---
+++ frontend/src/app/services/auth-service.ts
@@ -0,0 +1,80 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Login, LoginResponse, Register } from '../interfaces/auth';
+import { Observable, tap } from 'rxjs';
+import { UserProfile } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  // Inyecciones de dependencias
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  // Guarda el token de acceso en una señal reactiva temporalmente
+  accessToken = signal<string | null>(null);
+  currentUser = signal<UserProfile | null>(null);
+
+  // Método para registrar un nuevo usuario
+  register(data: Register): Observable<any> {
+    return this.http.post(`${this.apiUrl}/users/register/`, data);
+  }
+
+  // Método para iniciar session
+  login(data: Login): Observable<LoginResponse> {
+    return this.http
+      .post<LoginResponse>(`${this.apiUrl}/users/login/`, data, {
+        // Asegura que las cookies de sesión se incluyan en la solicitud
+        withCredentials: true,
+      })
+      .pipe(
+        // Guarda el token de acceso en la señal reactiva cuando se recibe la respuesta, solo en memoria
+        tap((response) => {
+          this.accessToken.set(response.access);
+        }),
+      );
+  }
+
+  // Método para refrescar el token
+  refreshToken(): Observable<LoginResponse> {
+    return this.http
+      .post<LoginResponse>(
+        `${this.apiUrl}/users/refresh/`,
+        {},
+        {
+          withCredentials: true,
+        },
+      )
+      .pipe(
+        tap((response) => {
+          this.accessToken.set(response.access);
+        }),
+      );
+  }
+
+  // Método para cerrar sesión
+  logout(): Observable<any> {
+    return this.http
+      .post(
+        `${this.apiUrl}/users/logout/`,
+        {},
+        {
+          withCredentials: true,
+        },
+      )
+      .pipe(
+        tap(() => {
+          // Limpia la memoria
+          this.accessToken.set(null);
+          this.currentUser.set(null);
+        }),
+      );
+  }
+
+  // Método para verificar si existe un token en memoria
+  isAuthenticated(): boolean {
+    return !!this.accessToken();
+  }
+}
